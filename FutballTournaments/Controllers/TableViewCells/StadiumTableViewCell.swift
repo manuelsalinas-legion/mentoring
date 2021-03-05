3c03a47a72f036f2f6122907d2d6675e61c0b3c8
Cristian Plascencia <cristianp@bluetrailsoft.com>
2021-03-05 15:27:46 -0600

    Try to change image stadium, not completed

--- FutballTournaments/Controllers/TableViewCells/StadiumTableViewCell.swift
+++ FutballTournaments/Controllers/TableViewCells/StadiumTableViewCell.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class StadiumTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var imageStadium: UIImageView!
+//    @IBOutlet weak var imageStadium: UIImageView!
+    @IBOutlet weak var imageViewStadium: UIImageView!
     @IBOutlet weak var labelStrStadium: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,23 +20,20 @@ class StadiumTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
+        
         // Configure the view for the selected state
-        self.imageStadium.clipsToBounds = true
+//        self.imageStadium.clipsToBounds = true
     }
     
     
     func loadInfo(_ teamDetails: TeamDetails?) {
         if let imageURL = URL(string: teamDetails?.strStadiumThumb ?? "") {
-            self.imageStadium.loadRemote(url: imageURL)
+            self.imageViewStadium.loadRemote(url: imageURL)
         } else {
-            self.imageStadium?.image = UIImage(named: "placeholderPicture")
+            self.imageViewStadium?.image = UIImage(named: "placeholderPicture")
         }
-        
-        if let strStadium = teamDetails?.strStadium {
-            self.labelStrStadium.text = strStadium
-        } else {
-            self.labelStrStadium.text = "Uknown"
-        }
+
+        self.labelStrStadium.text = teamDetails?.strStadium ?? "Uknown"
     }
 }
